951ba09f8bdd7c1916b41718caf7f3997b5fa1fc
Pu Anlai <inspectormustache@users.noreply.github.com>
2018-03-17 17:24:55 +0100

    Add basic functionality tests

--- tests/test_maninex.py
+++ tests/test_maninex.py
@@ -1,7 +1,9 @@
 import os
+import re
 import tempfile
 import shutil
 import configparser
+from threading import Thread
 import pytest
 from maninex import maninex
 
@@ -18,7 +20,7 @@ egnjhciaieeiiohknchakcodbpgjnchh
 
 
 @pytest.fixture(scope='module')
-def confix_struct():
+def config_struct():
     test_json_dir = tempfile.mkdtemp(prefix='mt_json_')
     test_ext_dir = tempfile.mkdtemp(prefix='mt_ext_')
     _, test_config_file = tempfile.mkstemp(prefix='mt_conf_')
@@ -28,60 +30,87 @@ def confix_struct():
 
     config = configparser.ConfigParser(allow_no_value=True)
     config.optionxform = lambda option: option
+    config.read(test_config_file)
 
     yield maninex.Configs(json_dir=test_json_dir, ext_dir=test_ext_dir,
                           config_file=test_config_file,
-                          config=config.read(test_config_file))
+                          config=config)
 
     shutil.rmtree(test_json_dir)
     shutil.rmtree(test_ext_dir)
     os.remove(test_config_file)
 
 
-def test_clean_mode():
-    try:
-        maninex.clean_mode()
-    except SystemExit:
-        pass
+def test_file_getters(config_struct):
+    """Test get_* functions with no installed extensions."""
+    installed_jsons = maninex.get_existing_jsons(config_struct.json_dir)
+    installed_folders = maninex.get_existing_folders(config_struct.ext_dir)
+    assert len(installed_jsons) == 0
+    assert len(installed_folders) == 0
+    for ext_ref in maninex.get_exts_from_config(config_struct.config):
+        assert not maninex.is_installed(config_struct,
+                                        ext_ref.idstr)
 
 
-def test_install_mode():
-    try:
-        maninex.install_mode()
-    except SystemExit:
-        pass
+def test_install_extensions(config_struct):
+    """Try installing extensions and check if the get_ functions return
+    appropriate results."""
+    exts = maninex.get_exts_from_config(config_struct.config)
+
+    threads = []
+    for ext_ref in exts:
+        ext_thread = Thread(target=maninex.process_extension_install,
+                            args=(config_struct, ext_ref))
+        ext_thread.start()
+        threads.append(ext_thread)
+
+    for ext_thread in threads:
+        ext_thread.join()
+
+    for ext_ref in exts:
+        assert maninex.is_installed(config_struct,
+                                    ext_ref.idstr)
+        local_version = maninex.get_local_version(config_struct.ext_dir,
+                                                  ext_ref.idstr)
+        online_version = maninex.ExtensionOnline(config_struct,
+                                                 ext_ref.idstr).version
+        assert local_version == online_version
 
 
-def test_list_mode():
-    try:
-        maninex.list_mode()
-    except SystemExit:
-        pass
+def test_update_extensions(config_struct):
+    """Assume all extensions are outdated and try updating them."""
+    exts = maninex.get_exts_from_config(config_struct.config)
 
+    for ext_ref in exts:
+        ext_obj = maninex.ExtensionOnline(config_struct, ext_ref.idstr)
+        # rename the extension files, so they appear out of date
+        filepath = ext_obj.ext_path_file
+        new_filename = re.sub(r'(\d[\d_]+)', 'foo', os.path.basename(filepath))
+        new_filepath = os.path.join(os.path.dirname(filepath), new_filename)
+        os.rename(filepath, new_filepath)
 
-def test_remove_mode():
-    try:
-        maninex.remove_mode()
-    except SystemExit:
-        pass
+    threads = []
+    dir_list = maninex.get_existing_folders(config_struct.ext_dir)
+    for ext_ref in exts:
+        ext_thread = Thread(target=maninex.process_extension_update,
+                            args=(config_struct, ext_ref, dir_list))
+        ext_thread.start()
+        threads.append(ext_thread)
 
+    for ext_thread in threads:
+        ext_thread.join()
 
-def test_scan_mode():
-    try:
-        maninex.scan_mode()
-    except SystemExit:
-        pass
-
-
-def test_update_mode():
-    try:
-        maninex.update_mode()
-    except SystemExit:
-        pass
-
-
-def test_print_skel_mode():
-    try:
-        maninex.print_skel_mode()
-    except SystemExit:
-        pass
+    for ext_ref in exts:
+        assert maninex.is_installed(config_struct,
+                                    ext_ref.idstr)
+        ext_obj = maninex.ExtensionOnline(config_struct, ext_ref.idstr)
+        local_version = maninex.get_local_version(config_struct.ext_dir,
+                                                  ext_ref.idstr)
+        assert local_version == ext_obj.version
+        # assume there's an .old backup file for every extension in their
+        # respective directory
+        for f in os.scandir(ext_obj.ext_path):
+            if f.name.endswith('.foo.old'):
+                break
+        else:
+            raise FileNotFoundError
